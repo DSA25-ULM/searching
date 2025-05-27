ef94db014ea498ae19aa4328ce3e9a43d92906a6
Minamoto Yuki <69187285+MinamotoYuki46@users.noreply.github.com>
2025-05-27 14:05:59 +0800

    some fix

---
+++ searching.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <conio.h>
+#include <random>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+void sequentialSearch(const vector<int> &nums){
+    // TODO
+}
+
+void binarySearch(const vector<int> &nums){
+    // TODO
+}
+
+/// use this to clear screen, change if necessary
+void clearScreen(){
+    system("cls");
+}
+
+// use this to  print your answer
+void explain(){
+
+}
+
+
+int main() {
+    int opt;
+    do {
+        cout << "Pilih menu" << endl;
+        cout << "1. Sequential Searching" << endl;
+        cout << "2. Binary Searching" << endl;
+        cout << "3. Jelaskan Perbedaan Sequential Searching dan Binary Searching!" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Pilih: ";
+        cin >> opt;
+
+        switch (opt) {
+            case 1: {
+                vector<int> nums (100);
+                mt19937_64 rng(random_device{}());
+                uniform_int_distribution<int> dist(1, 50);
+
+                for (auto &val: nums) {
+                    val = (dist(rng));
+                }
+
+                cout << "100 angka acak telah digenerate.\n";
+                sequentialSearch(nums);
+                break;
+            }
+
+            case 2: {
+                int size;
+                cout << "Masukkan ukuran vector: ";
+                cin >> size;
+
+                vector<int> nums(size);
+                mt19937_64 rng(random_device{}());
+                uniform_int_distribution<int> dist(1, 100);
+
+                for (auto &val: nums) {
+                    val = (dist(rng));
+                }
+
+                sort(nums.begin(), nums.end());
+                cout << size << " angka telah digenerate dan diurutkan.\n";
+                binarySearch(nums);
+                break;
+            }
+
+            case 3:
+                explain();
+                break;
+
+            case 4:
+                cout << "\nTERIMA KASIH\n";
+                cout << "Programme was made by Fulan (24xxxxxxxxxxx)" << endl;
+                break;
+
+            default:
+                cout << "Opsi tidak terdefinisi, mohon masukkan ulang opsi" << endl;
+                break;
+        }
+
+        if (opt != 4) {
+            cout << "\nTekan sembarang tombol untuk melanjutkan...";
+            getch();
+            clearScreen();
+        }
+
+    } while (opt != 4);
+
+    return 0;
+}
